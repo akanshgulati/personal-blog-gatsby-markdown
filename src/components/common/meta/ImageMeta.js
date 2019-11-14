6a12a175e031800b4916dcb6f7d37decca2f27bb
Akansh Gulati <akanshgulati@gmail.com>
2019-11-15 01:18:56 +0530

    fix(ImageMeta): Add site info for twitter

--- src/components/common/meta/ImageMeta.js
+++ src/components/common/meta/ImageMeta.js
@@ -11,6 +11,7 @@ const ImageMeta = ({ image }) => {
     return (
         <Helmet>
             <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@akanshgulati" />
             <meta name="twitter:image" content={image} />
             <meta property="og:image" content={image} />
             <meta property="og:image:width" content={config.shareImageWidth} />
